ee89eece714bf9c13fb633a4b5c1569ef024d8f5
Rubens Filipe <rubensfilipe6@gmail.com>
2020-04-01 16:50:30 -0300

    add loadPageAuto

--- assets/js/myconfig.js
+++ assets/js/myconfig.js
@@ -1,3 +1,35 @@
+window.morePostLoading = false;
+
+function loadMorePosts(){
+	if(window.morePostLoading == false){
+
+		window.morePostLoading = true;
+
+		jQuery('.j_btn').hide();
+	
+		var offset = jQuery('.post_item').length;
+		jQuery.ajax({
+			type:'POST',
+			url: ajaxUrl,
+			data: { action:'LoadMorePosts', offset: offset },
+			success: function(html){
+				
+				jQuery('.j_btn').show()
+				
+				if( html != '' ){
+					jQuery('.post_content_more').append(html);
+				}else{
+					jQuery('.j_btn').hide()
+				}
+
+				window.morePostLoading = false;
+			}
+		});
+	}
+	
+	
+}
+
 jQuery(function(){
 
 	jQuery('.j_back').on('click', function(){
@@ -8,28 +40,16 @@ jQuery(function(){
 		jQuery(".collapse").toggle("slow");
 	});
 
-	function loadMorePosts(){
-		
-		jQuery('.j_btn').hide();
-		
-		var offset = jQuery('.post_item').length;
-		jQuery.ajax({
-			type:'POST',
-			url: ajaxUrl,
-			data: { action:'LoadMorePosts', offset: offset },
-			success: function(html){
-				jQuery('.j_btn').show()
-				
-				if( html != '' ){
-					jQuery('.post_content_more').append(html);
-				}else{
-					jQuery('.j_btn').hide()
-				}
-			}
-		});
-		
-	}
-
 	jQuery('.j_btn').on('click', loadMorePosts);
+
+	jQuery(window).on('scroll', function(){
+		//WINDOW.SCROLLY = POSICAO ATUAL DO SCROLL
+		//DOCUMENT.BODY.SCROLLHEIGHT = ALTURA ATUAL DO SITE
+		var porcent = ( window.scrollY / document.body.scrollHeight ) * 100;
+		// console.log(porcent);
+		if( porcent >= 40 ){
+			loadMorePosts();
+		}
+	});
 
 });
